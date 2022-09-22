26976d65943e219be68d2382027e5ce1831fba0f
Damien Laitani <d.laitani@gmail.com>
2022-09-22 17:58:04 +0200

    update value input and use old()

--- backoffice/resources/views/category/add.tpl.php
+++ backoffice/resources/views/category/add.tpl.php
@@ -6,12 +6,12 @@
         <?= csrf_field() ?>
         <div class="mb-3">
             <label for="name" class="form-label">Nom</label>
-            <input type="text" name="name" class="form-control" id="name" placeholder="Nom de la catégorie" value="<?= isset($old_inputs['name']) ? $old_inputs['name'] : '' ?>">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Nom de la catégorie" value="<?= old('name') ?>">
             <p class="text-danger"><?= $errors_messages['name'][0] ?? '' ?></p>
         </div>
         <div class="mb-3">
             <label for="subtitle" class="form-label">Sous-titre</label>
-            <input type="text" name="subtitle" class="form-control" id="subtitle" placeholder="Sous-titre" value="<?= isset($old_inputs['subtitle']) ? $old_inputs['subtitle'] : '' ?>" aria-describedby="subtitleHelpBlock">
+            <input type="text" name="subtitle" class="form-control" id="subtitle" placeholder="Sous-titre" value="<?= old('subtitle') ?>" aria-describedby="subtitleHelpBlock">
             <small id="subtitleHelpBlock" class="form-text text-muted">
                 Sera affiché sur la page d'accueil comme bouton devant l'image
             </small>
@@ -19,7 +19,7 @@
         </div>
         <div class="mb-3">
             <label for="picture" class="form-label">Image</label>
-            <input type="text" name="picture" class="form-control" id="picture" placeholder="image jpg, gif, svg, png" aria-describedby="pictureHelpBlock" value="<?= isset($old_inputs['picture']) ? $old_inputs['picture'] : '' ?>">
+            <input type="text" name="picture" class="form-control" id="picture" placeholder="image jpg, gif, svg, png" aria-describedby="pictureHelpBlock" value="<?= old('picture') ?>">
             <small id="pictureHelpBlock" class="form-text text-muted">
                 URL relative d'une image (jpg, gif, svg ou png) fournie sur <a href="" target="_blank">cette page</a>
             </small>
