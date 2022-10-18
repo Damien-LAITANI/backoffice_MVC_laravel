404cd1854bd88125a04335447b28bf36e7055f87
Damien Laitani <d.laitani@gmail.com>
2022-10-18 16:49:20 +0200

    add errors 403 view

---
+++ backoffice/resources/views/errors/403.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BackOffice - Bedacier</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+<div class="container-fluid w-75 mt-5">
+    <h1 class="display-4">Erreur 403 </h1>
+    <h3>Vous ne disposez pas des autorisations nécessaires pour effectuer cette action</h3>
+    <p>Pour retournez à <a href="<?= route('home') ?>">l'accueil</a>.</p>
+</div>
+
